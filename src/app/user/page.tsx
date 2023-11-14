c9dff092f7bd3840f364520f42d92c5a56c8777b
Anh Dao <daoanh440@gmail.com>
2023-11-15 10:28:50 +1100

    test

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,20 +1,82 @@
 // pages/user/[userID].js
 
 'use client'
-import { useSearchParams } from 'next/navigation'
+import {useEffect, useState} from "react";
 
-const UserPage = () => {
-    const searchParams = useSearchParams()
+const IframePage = () => {
 
-    const userID = searchParams.get('userID')
-    const recentlyCompletedObjective = searchParams.get('questID')
+    const [messageCount, setMessageCount] = useState(0)
+    const [questOneDone, setQuestOneDone] = useState(false)
+    const [questTwoDone, setQuestTwoDone] = useState(false)
+    const [questThreeDone, setQuestThreeDone] = useState(false)
+
+
+    let index = 0;
+
+    const handleReceiveMessage = (event: MessageEvent) => {
+        // event.data contains the received message
+        const receivedMessage = event.data;
+        index += 1;
+        setMessageCount(index)
+        console.log(receivedMessage)
+        // complete quest
+        if (receivedMessage === "1") {
+            setQuestOneDone(true)
+        }
+        if (receivedMessage === "2") {
+            setQuestTwoDone(true)
+        }
+        if (receivedMessage === "3") {
+            setQuestThreeDone(true)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('message', handleReceiveMessage);
+        return () => {
+            window.removeEventListener('message', handleReceiveMessage);
+        };
+    }, []);
+
+    const handleSendParentMessage = () => {
+        // Called from the iframe
+        const message = JSON.stringify({
+            message: 'Hello from iframe',
+            date: Date.now(),
+        });
+        window.parent.postMessage(message, '*');
+    }
+
+    const handleSendOpenWindowMessage= () => {
+        // Called from the iframe
+        const message = JSON.stringify({
+            open:true,
+            message: 'open RecycleBin',
+            date: Date.now(),
+        });
+        window.parent.postMessage(message, '*');
+    }
+
+
 
     return (
-        <div className={"text-2xl bg-white"}>
-            <p>User ID: {userID || "no User ID"}</p>
-            <p>Recently Completed Objective: {recentlyCompletedObjective || "Not yet Started"}</p>
+        <div style={{padding:"10px"}}>
+            <button onClick={handleSendParentMessage}>Send Parent Message</button>
+            <button onClick={handleSendOpenWindowMessage}>Open Bin</button>
+            <p>Parent Message Count: {messageCount}</p>
+            <ol>
+                <li style={{
+                    textDecoration: questOneDone ? "line-through" : "none"
+                }}>Open My Computer</li>
+                <li style={{
+                    textDecoration: questTwoDone ? "line-through" : "none"
+                }}>Maximise My Computer</li>
+                <li style={{
+                    textDecoration: questThreeDone ? "line-through" : "none"
+                }}>Adjust My Computer window size</li>
+            </ol>
         </div>
-    );
-};
+    )
+}
 
-export default UserPage;
+export default IframePage
